ec764aebe5f802861c0acd9c5999790a2f6ef1f7
Kirsten Keels <keelsk@gmail.com>
2020-04-15 00:40:55 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student ="hey"
     redirect_to new_student_path
   end
 
